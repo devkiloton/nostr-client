6425caae14d818ebca0a4c7322eb0563477f378e
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-30 01:00:31 -0300

    Loggin events

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Event, SimplePool } from 'nostr-tools'
+import { RELAYS } from './constants/relays'
 
 function App() {
+
+  const [pool, setPool] = useState<SimplePool | null>(null)
+
+  /**
+   * Settin up relays pool
+   */
+  useEffect(() => {
+    const _pool = new SimplePool()
+    setPool(_pool)
+  
+    return () => {
+      _pool.close(RELAYS)
+    }
+  }, [])
+
+  /**
+   * Subscribing to events
+   */
+  useEffect(() => {
+    if (!pool) return
+
+    const sub = pool.sub(RELAYS, [{
+      kinds: [1],
+      limit: 100,
+      // Filtering words
+      '#t': ['https']
+    }])
+
+    sub.on('event', (data: Event) => {
+      console.log('data', data)
+    })
+
+    return () => {
+      sub.unsub()
+    }
+  }, [pool])
 
   return (
     <h1 className="text-3xl font-bold underline">
