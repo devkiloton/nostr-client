fa96b2cd74ada897824ae9e1e53bf5695eababaa
Yarego Brozek <dev.kiloton@gmail.com>
2023-10-31 02:04:08 -0300

    Removing unfunctional realys

--- src/constants/relays.ts
+++ src/constants/relays.ts
@@ -1,5 +1,1 @@
-export const RELAYS = [
-  "wss://nostr-pub.wellorder.net",
-  "wss://nostr.drss.io",
-  "wss://relay.damus.io",
-];
+export const RELAYS = ["wss://nostr-pub.wellorder.net", "wss://relay.damus.io"];
