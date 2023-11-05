20c3ac84be507e48bb3cec5af45b567875a87b5b
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-05 20:33:25 -0300

    Little refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,10 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
-import { Event, SimplePool } from 'nostr-tools'
-import { RELAYS } from './constants/relays'
-import { NotesList } from './components/NotesList'
-import { MetadataPbKey } from './types/MetadataPbKey'
-import { binaryTreeDescendingDate } from './helpers/binaryTreeDescendingDate'
-import { useDebounce } from 'use-debounce'
-import CreateNote from './components/CreateNote'
-import HashtagsFilter from './components/HashTagsFilter'
+import { Home } from './pages/Home'
 
 
 function App() {
-  const [pool, setPool] = useState<SimplePool | null>(null)
-  const [eventsImmediate, setEvents] = useState<Event[]>([])
-  const [metadata, setMetadata] = useState<Record<string, MetadataPbKey>>({})
-  const [hashtags, setHashtags] = useState<string[]>([])
-  const [events] = useDebounce(eventsImmediate, 400)
-  const metadataFetched = useRef<Record<string, boolean>>({})
-
-  /**
-   * Settin up relays pool
-   */
-  useEffect(() => {
-    const _pool = new SimplePool()
-    setPool(_pool)
-  
-    return () => {
-      _pool.close(RELAYS)
-    }
-  }, [])
-
-  /**
-   * Subscribing to events
-   */
-  useEffect(() => {
-    if (!pool) return
-    setEvents([])
-    const sub = pool.sub(RELAYS, [{
-      kinds: [1],
-      limit: 100,
-      '#t': hashtags
-    }])
-
-    sub.on('event', (data: Event) => {
-      setEvents((curr) => binaryTreeDescendingDate(curr, data))
-    })
-
-    return () => {
-      sub.unsub()
-    }
-  }, [hashtags,pool])
-
-  useEffect(() => {
-    if (!pool) return;
-
-    const pubKeysToFetch = events.filter((event) => metadataFetched.current[event.pubkey] === true).map((event) => event.pubkey)
-   pubKeysToFetch.forEach((pubKey) => metadataFetched.current[pubKey] = true)
-
-    const sub = pool.sub(RELAYS, [{
-      kinds: [0],
-      authors: pubKeysToFetch,
-    }])
-
-    sub.on('event', (data: Event) => {
-      const metadata = JSON.parse(data.content) as MetadataPbKey
-
-      setMetadata((curr) => ({
-        ...curr,
-        [data.pubkey]: metadata,
-      }))
-    })
-
-
-    // eose = end of stored events
-    sub.on('eose', () => { 
-      sub.unsub()
-    })
-
-    return () => { }
-  }, [events, pool])
-
-  if(!pool) return <h1>Loading...</h1>
 
   return (
-    <>
-    <h1 className='text-start font-bold mb-4'>Nostr Feed</h1>
-      <CreateNote hashtags={hashtags} pool={pool} />
-      <HashtagsFilter hashtags={hashtags} onChange={setHashtags}/>
-    <NotesList metadataPbKey={metadata} notes={events} />
-    </>
+    <Home/>
   )
 }
 
--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -6,18 +6,20 @@ import { NoteCard } from "./NoteCard";
 export const NotesList = ({ notes, metadataPbKey }: NotesListProps) => {
     return (
         <div className= "flex flex-col gap-4" >
-        {notes.map((note) => (
-            <NoteCard
-                key={crypto.randomUUID()}
-                created_at={note.created_at}
-                user={{
-                    name: metadataPbKey[note.pubkey]?.name ??  `${nip19.npubEncode(note.pubkey).slice(0,12)}...`,
-                    picture: metadataPbKey[note.pubkey]?.picture ?? `https://api.dicebear.com/5.x/identicon/svg?seed=${note.pubkey}`,
-                    pubKey: note.pubkey,
-                }}
-                hashtags={note.tags.filter((hashtag) => hashtag[0] === 't').map((hashtag) => hashtag[1])}
-                content={note.content} />
-        ))}
+            {notes.map((note) => {
+                // console.log(metadataPbKey)
+                return ( <NoteCard
+                    key={crypto.randomUUID()}
+                    created_at={note.created_at}
+                    user={{
+                        name: metadataPbKey[note.pubkey]?.name ??  `${nip19.npubEncode(note.pubkey).slice(0,12)}...`,
+                        picture: metadataPbKey[note.pubkey]?.picture ?? `https://api.dicebear.com/5.x/identicon/svg?seed=${note.pubkey}`,
+                        pubKey: note.pubkey,
+                    }}
+                    hashtags={note.tags.filter((hashtag) => hashtag[0] === 't').map((hashtag) => hashtag[1])}
+                    content={note.content} />
+            )
+            })}
         </div>
     )
 }
